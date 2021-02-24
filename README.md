bbb594023338cc85d747c20bd5880b013c107993
8-u8 <itotaka523@gmail.com>
2021-02-24 22:08:46 +0900

    progressed code.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # What_can_be_Computed
+- [これを勉強する](https://www.amazon.co.jp/dp/4873119332/?coliid=I418VGRC5XB8Q&colid=17MC75ZCCJ72K&psc=1&ref_=lv_ov_lig_dp_it)
---
+++ containsGAGA.py
@@ -0,0 +1,12 @@
+#%%
+import utils
+## no module named rf
+# from utils import rf
+
+#%%
+def containsGAGA(inString):
+    if 'GAGA' in inString:
+        return 'yes'
+    else:
+        return 'no'
+
